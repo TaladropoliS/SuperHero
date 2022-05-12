61fca81841a2d3671e86991d46bae857919cda87
Fernando Taladriz <fernando.taladriz@gmail.com>
2022-05-12 12:21:22 -0400

    colors

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,36 +1,55 @@
 $(function () {
-
     $('form').submit(function (event) {
         event.preventDefault()
+
         $('#msg').text('Cargando . . .')
         let valueInput = $('#inputHero').val();
 
-        $.ajax({
-            type: 'GET',
-            url: `https://www.superheroapi.com/api.php/4905856019427443/${valueInput}`,
-            success: function (data) {
-                $('#msg').hide()
+        //Validación que solo admite números en el form
+        var validateReg = /[0-9]/gim;
+        let validateID = validateReg.test(valueInput);
 
-                let imagen = data.image.url
-                let nombre = data.name
-                let conexiones = data.connections['group-affiliation']
-                let publicado = data.biography.publisher
-                let ocupacion = data.work.occupation
-                let primeraAparicion = data.biography['first-appearance']
-                let altura = data.appearance.height[1]
-                let peso = data.appearance.weight[1]
-                let aliados = data.biography.aliases
-                let aliadosList = []
+        if (!valueInput) {
+            $('#msgInput').text('Por favor ingresa un número.')
+            // alert("Por favor ingresa un número.");
+        } else {
+            if (!validateID) {
+                $('#msgInput').text('Por favor ingresa sólo números.')
+                // alert("Por favor ingresa sólo números.");
+            }
+            if (valueInput < 1 || valueInput > 732) {
+                $('#msgInput').text('Por favor ingresa números del 1 al 732.')
+                // alert("Por favor ingresa números del 1 al 732.");
+            }
+            $('#msgInput').text('')
+            $('#respuesta').removeClass('d-none')
 
-                for (let i of aliados) {
-                    i = ' ' + i
-                    aliadosList.push(i)
-                }
+            $.ajax({
+                type: 'GET',
+                url: `https://www.superheroapi.com/api.php/4905856019427443/${valueInput}`,
+                success: function (data) {
+                    $('#msg').hide()
 
-                $('#superHeroSection').html(`
-                <div class="card">
+                    let imagen = data.image.url
+                    let nombre = data.name
+                    let conexiones = data.connections['group-affiliation']
+                    let publicado = data.biography.publisher
+                    let ocupacion = data.work.occupation
+                    let primeraAparicion = data.biography['first-appearance']
+                    let altura = data.appearance.height[1]
+                    let peso = data.appearance.weight[1]
+                    let aliados = data.biography.aliases
+                    let aliadosList = []
+
+                    for (let i of aliados) {
+                        i = ' ' + i
+                        aliadosList.push(i)
+                    }
+
+                    $('#superHeroSection').html(`
+                <div class="card bg-transparent border-0">
                     <div class="row g-0">
-                        <div class="col-md-4">
+                        <div class="col-md-4 my-auto">
                             <img src="${imagen}" class="img-fluid rounded-start" alt="...">
                         </div>
                         <div class="col-md-8">
@@ -55,41 +74,73 @@ $(function () {
                 </div>
                 `)
 
-                let combat = parseInt(data.powerstats.combat)
-                let durability = parseInt(data.powerstats.durability)
-                let intelligence = parseInt(data.powerstats.intelligence)
-                let power = parseInt(data.powerstats.power)
-                let speed = parseInt(data.powerstats.speed)
-                let strength = parseInt(data.powerstats.strength)
+                    let combat = parseInt(data.powerstats.combat)
+                    console.log(combat)
+                    if(!combat){
+                        combat = 0
+                    }
+                    console.log(combat)
+                    let durability = parseInt(data.powerstats.durability)
+                    console.log(durability)
+                    if(!durability){
+                        durability = 0
+                    }
+                    console.log(durability)
+                    let intelligence = parseInt(data.powerstats.intelligence)
+                    console.log(intelligence)
+                    if(!intelligence){
+                        intelligence = 0
+                    }
+                    console.log(intelligence)
+                    let power = parseInt(data.powerstats.power)
+                    console.log(power)
+                    if(!power){
+                        power = 0
+                    }
+                    console.log(power)
+                    let speed = parseInt(data.powerstats.speed)
+                    console.log(speed)
+                    if(!speed){
+                        speed = 0
+                    }
+                    console.log(speed)
+                    let strength = parseInt(data.powerstats.strength)
+                    console.log(strength)
+                    if(!strength){
+                        strength = 0
+                    }
+                    console.log(strength)
 
-                var chart = new CanvasJS.Chart("chartContainer", {
-                    theme: "light2", // "light1", "light2", "dark1", "dark2"
-                    exportEnabled: true,
-                    animationEnabled: true,
-                    title: {
-                        text: `${nombre}`
-                    },
-                    data: [{
-                        type: "pie",
-                        startAngle: 25,
-                        toolTipContent: "<b>{label}</b>: {y}%",
-                        showInLegend: "true",
-                        legendText: "{label}",
-                        indexLabelFontSize: 13,
-                        indexLabel: "{label} - {y}%",
-                        dataPoints: [
-                            {y: combat, label: "combat"},
-                            {y: durability, label: "durability"},
-                            {y: intelligence, label: "intelligence"},
-                            {y: power, label: "power"},
-                            {y: speed, label: "speed"},
-                            {y: strength, label: "strength"},
-                        ]
-                    }]
-                });
-                chart.render();
-            }
-        });
+                    var chart = new CanvasJS.Chart("chartContainer", {
+                        theme: "dark2", // "light1", "light2", "dark1", "dark2"
+                        exportEnabled: true,
+                        animationEnabled: true,
+                        title: {
+                            text: `${nombre}`
+                        },
+                        data: [{
+                            type: "pie",
+                            startAngle: 25,
+                            toolTipContent: "<b>{label}</b>: {y}",
+                            showInLegend: "true",
+                            legendText: "{label}",
+                            indexLabelFontSize: 13,
+                            indexLabel: "{label} - {y}",
+                            dataPoints: [
+                                {y: combat, label: "combat"},
+                                {y: durability, label: "durability"},
+                                {y: intelligence, label: "intelligence"},
+                                {y: power, label: "power"},
+                                {y: speed, label: "speed"},
+                                {y: strength, label: "strength"},
+                            ]
+                        }]
+                    });
+                    chart.render();
+                }
+            });
+        }
+
     })
 
     // navbar ACTIVE
